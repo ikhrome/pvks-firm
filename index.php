5bceeb31c82a16689765a04f3bba85df144050ae
Ivan Khromov <khromov.ivan@gmail.com>
2019-05-28 14:08:44 +0500

    Исправление ошибки при пустом роуте

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 ob_start();
 require 'functions.php';
 $routes = require_once 'routes.php';
-$page = htmlspecialchars($_REQUEST['page']);
+$action = isset($_GET['route']) ? htmlspecialchars($_REQUEST['route']) : null;
 
-if($page == '' || $page == null) {
+if(empty($action) || $action == null) {
 	include 'pages/index.php';
-} elseif(isset($routes[$page])) {
-	include $routes[$page] . ".php";
+} elseif(isset($routes[$action])) {
+	include $routes[$action] . ".php";
 } else {
 	include "pages/404.php";
 }
